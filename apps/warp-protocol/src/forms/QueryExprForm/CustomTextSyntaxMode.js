bc0fb74ef39a782ce5575cd30c8908b51399478f
simke9445 <simke9445@gmail.com>
2022-12-20 02:14:43 +0100

    fmt

--- apps/warp-protocol/src/forms/QueryExprForm/CustomTextSyntaxMode.js
+++ apps/warp-protocol/src/forms/QueryExprForm/CustomTextSyntaxMode.js
@@ -1,20 +1,20 @@
 import 'brace/mode/text';
 
-export class CustomHighlightRules extends window.ace.acequire("ace/mode/text_highlight_rules").TextHighlightRules {
-	constructor() {
-		super();
-		this.$rules = {
-      "start": [
+export class CustomHighlightRules extends window.ace.acequire('ace/mode/text_highlight_rules').TextHighlightRules {
+  constructor() {
+    super();
+    this.$rules = {
+      start: [
         // highlight text between curly braces with the 'highlighted_text' class
-        { regex: /{(\w|\s)+}/, token: "highlighted_text" },
-      ]
-		};
-	}
+        { regex: /{(\w|\s)+}/, token: 'highlighted_text' },
+      ],
+    };
+  }
 }
 
 export default class CustomTextSyntaxMode extends window.ace.acequire('ace/mode/text').Mode {
-	constructor() {
-		super();
-		this.HighlightRules = CustomHighlightRules;
-	}
+  constructor() {
+    super();
+    this.HighlightRules = CustomHighlightRules;
+  }
 }
