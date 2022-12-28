db05d3e32472d1cdf61740d875ae6e9028eb78a3
simke9445 <simke9445@gmail.com>
2022-12-28 15:47:51 +0100

    job page changes

--- apps/warp-protocol/src/pages/job-page/panels/Condition/ConditionTree.tsx
+++ apps/warp-protocol/src/pages/job-page/panels/Condition/ConditionTree.tsx
@@ -5,22 +5,24 @@ import { Condition } from './Conditon';
 import { ReactNode } from 'react';
 import { UIElementProps } from '@terra-money/apps/components';
 import classNames from 'classnames';
+import { Job } from 'types/job';
 
 export type ConditionTreeProps = {
   title: ReactNode;
   items: warp_controller.Condition[];
   isRoot?: boolean;
+  job: Job;
 } & UIElementProps;
 
 export const ConditionTree = (props: ConditionTreeProps) => {
-  const { title, items, className, isRoot } = props;
+  const { title, items, className, isRoot, job } = props;
 
   return (
     <div className={classNames(className, { [styles.item]: !isRoot })}>
       <li className={classNames(styles.tree_label, { [styles.first_label]: isRoot })}>{title}</li>
       <ul className={styles.list}>
         {items.map((item, index) => (
-          <Condition className={styles.item} condition={item} />
+          <Condition className={styles.item} condition={item} job={job} />
         ))}
       </ul>
     </div>
