cb8ba8f1a9939e02dae0a495df52c86fa09bcb24
simke9445 <simke9445@gmail.com>
2022-12-30 18:34:24 +0100

    useTemplatesQuery placeholder

--- apps/warp-protocol/src/queries/useTemplatesQuery.ts
+++ apps/warp-protocol/src/queries/useTemplatesQuery.ts
@@ -3,8 +3,32 @@ import { CW20Addr } from '@terra-money/apps/types';
 import { NetworkInfo, useConnectedWallet } from '@terra-money/wallet-provider';
 import { useQuery, UseQueryResult } from 'react-query';
 import { warp_controller } from 'types';
+import jsonpath from 'jsonpath';
 import { QUERY_KEY } from './queryKey';
 import { contractQuery } from '@terra-money/apps/queries';
+
+// TODO: remove this / temporary workaround
+const composeMsgFromTemplate = (template: warp_controller.Template, vars: TmpTemplateVar[]): string => {
+  let json = JSON.parse(template.msg);
+
+  vars.forEach((v) => {
+    try {
+      // TODO: add paths
+      jsonpath.value(json, v.path, `$warp.variable.${v.name}`);
+    } catch (err) {
+      // consume the error
+    }
+  });
+
+  return JSON.stringify(json, null, 2);
+};
+
+type TmpTemplateVar = {
+  name: string;
+  kind: warp_controller.VariableKind;
+  path: string;
+  default_value: string;
+};
 
 const fetchTemplates = async (
   network: NetworkInfo,
@@ -16,7 +40,25 @@ const fetchTemplates = async (
     warp_controller.TemplatesResponse
   >(network, contractAddress, { query_templates: opts }, { templates: [] });
 
-  return response.templates;
+  const resp = response.templates.map((t) => {
+    let newTemplate = { ...t };
+
+    newTemplate.vars = newTemplate.vars.map((v) => {
+      let newVar = v as any as TmpTemplateVar;
+      let pathSplits = newVar.path.split('.');
+      newVar.name = pathSplits[pathSplits.length - 1];
+      newVar.default_value = '';
+      return newVar;
+    }) as any;
+
+    newTemplate.msg = composeMsgFromTemplate(newTemplate, newTemplate.vars as any as TmpTemplateVar[]);
+
+    newTemplate.vars = newTemplate.vars.map((v) => ({ static: v } as any));
+
+    return newTemplate;
+  });
+
+  return resp;
 };
 
 type TemplatesQueryOpts = warp_controller.QueryTemplatesMsg & {
