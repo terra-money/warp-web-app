14ae23720a741bdad0e61794460eea799430644f
simke9445 <simke9445@gmail.com>
2023-01-02 18:16:51 +0100

    store cached varibles in local storage

--- apps/warp-protocol/src/pages/job-new/useCachedVariables.ts
+++ apps/warp-protocol/src/pages/job-new/useCachedVariables.ts
@@ -1,31 +1,20 @@
 import { useEffectOnceWhen } from '@terra-money/apps/hooks';
 import { useConnectedWallet } from '@terra-money/wallet-provider';
-import { useVariableStorage } from 'pages/variables/useVariableStorage';
-import { useCallback, useMemo, useState } from 'react';
+import { useVariableStorage, Variable } from 'pages/variables/useVariableStorage';
+import { useCallback, useMemo } from 'react';
 import { isEmpty } from 'lodash';
-import { warp_controller } from 'types/contracts/warp_controller';
 import { variableName } from 'utils/variable';
-
-export type Variable =
-  | warp_controller.Variable
-  | {
-      query: QueryVariable;
-    };
-
-export type QueryVariable = warp_controller.QueryVariable & {
-  template?: warp_controller.Template;
-  default_value: warp_controller.QueryExpr;
-};
+import { useLocalStorage } from 'usehooks-ts';
 
 export const useCachedVariables = () => {
   const connectedWallet = useConnectedWallet();
 
   const { variables: storageVars } = useVariableStorage();
-  const [cachedVariables, setCachedVariables] = useState<Variable[]>(storageVars);
+  const [cachedVariables, setCachedVariables] = useLocalStorage<Variable[]>('__warp_cached_variables', []);
 
   useEffectOnceWhen(
     () => setCachedVariables(storageVars),
-    () => !isEmpty(storageVars)
+    () => !isEmpty(storageVars) && isEmpty(cachedVariables)
   );
 
   const setVariables = useCallback(
@@ -54,12 +43,14 @@ export const useCachedVariables = () => {
     [setVariables]
   );
 
+  const clearAll = useCallback(() => {
+    setVariables([]);
+  }, [setVariables]);
+
   const saveVariable = useCallback(
     (variable: Variable, prev?: Variable) => {
       const variableExists = Boolean(variables.find((v) => variableName(v) === variableName(variable)));
       let updatedVariables = [];
-
-      console.log({ variable });
 
       if (!variableExists) {
         updatedVariables = [...variables, variable];
@@ -72,8 +63,6 @@ export const useCachedVariables = () => {
       if (prev && variableName(variable) !== variableName(prev)) {
         updatedVariables = updatedVariables.filter((v) => variableName(v) !== variableName(prev));
       }
-
-      console.log({ updatedVariables });
 
       setVariables(updatedVariables);
     },
@@ -93,7 +82,8 @@ export const useCachedVariables = () => {
       saveVariable,
       removeVariable,
       saveAll,
+      clearAll,
     }),
-    [variables, saveVariable, removeVariable, saveAll]
+    [variables, saveVariable, removeVariable, saveAll, clearAll]
   );
 };
