288cb177f58afcb2807f190a98adf2bf81317de8
simke9445 <simke9445@gmail.com>
2023-01-24 00:57:02 +0100

    add template fee

--- apps/warp-protocol/src/tx/useCreateTemplateTx.ts
+++ apps/warp-protocol/src/tx/useCreateTemplateTx.ts
@@ -1,5 +1,8 @@
 import { useContractAddress } from '@terra-money/apps/hooks';
 import { useTx, TxBuilder } from '@terra-money/apps/libs/transactions';
+import { useWarpConfig } from 'queries/useConfigQuery';
+import { useEffect, useRef } from 'react';
+import { LUNA } from 'types';
 import { warp_controller } from '../types/contracts';
 import { TX_KEY } from './txKey';
 
@@ -10,21 +13,40 @@ type SubmitTemplateMsg = Extract<warp_controller.ExecuteMsg, { submit_template: 
 export const useCreateTemplateTx = () => {
   const contractAddress = useContractAddress('warp-controller');
 
+  const { data: config } = useWarpConfig();
+
+  const configRef = useRef<warp_controller.Config | undefined>();
+
+  useEffect(() => {
+    configRef.current = config;
+  }, [config]);
+
   return useTx<CreateTemplateTxProps>(
     (options) => {
       const { wallet, formatted_str, kind, vars, name, msg, condition } = options;
 
+      if (!configRef.current) {
+        return { msgs: [] };
+      }
+
       return TxBuilder.new()
-        .execute<SubmitTemplateMsg>(wallet.walletAddress, contractAddress, {
-          submit_template: {
-            formatted_str,
-            kind,
-            condition,
-            vars,
-            name,
-            msg,
+        .execute<SubmitTemplateMsg>(
+          wallet.walletAddress,
+          contractAddress,
+          {
+            submit_template: {
+              formatted_str,
+              kind,
+              condition,
+              vars,
+              name,
+              msg,
+            },
           },
-        })
+          {
+            [LUNA.denom]: configRef.current.template_fee,
+          }
+        )
         .build();
     },
     {
