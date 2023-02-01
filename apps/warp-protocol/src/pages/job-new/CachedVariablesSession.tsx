deaf604e56b8e8e1141ddb8149db93bf855f8eed
simke9445 <simke9445@gmail.com>
2023-02-01 15:45:12 +0100

    add persistence on page refresh for cached variables session

--- apps/warp-protocol/src/pages/job-new/CachedVariablesSession.tsx
+++ apps/warp-protocol/src/pages/job-new/CachedVariablesSession.tsx
@@ -1,8 +1,9 @@
 import { useVariableStorage, Variable } from 'pages/variables/useVariableStorage';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 import { uniqBy } from 'lodash';
 import { useCachedVariables } from './useCachedVariables';
 import { variableName } from 'utils/variable';
+import { useEventListener } from 'usehooks-ts';
 
 interface CachedVariablesSessionProps {
   children: ReactNode;
@@ -15,18 +16,31 @@ export const CachedVariablesSession = (props: CachedVariablesSessionProps) => {
   const { variables: storageVars } = useVariableStorage();
   const { setVariables: setCachedVariables, variables: cachedVariables, clearAll } = useCachedVariables();
 
+  const refreshingPageRef = useRef<boolean>(false);
+  const clearAllRef = useRef<() => void>();
+
+  useEventListener('beforeunload', () => {
+    refreshingPageRef.current = true;
+  });
+
+  useEffect(() => {
+    return () => {
+      if (clearAllRef.current && !refreshingPageRef.current) {
+        clearAllRef.current();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    clearAllRef.current = clearAll;
+  }, [clearAll]);
+
   useEffect(() => {
     // TODO: check order
     const newVars = uniqBy([...cachedVariables, ...(input ?? []), ...storageVars], (v) => variableName(v));
     setCachedVariables(newVars);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [input]);
-
-  useEffect(() => {
-    return () => {
-      clearAll();
-    };
-  }, [clearAll]);
 
   return <>{children}</>;
 };
