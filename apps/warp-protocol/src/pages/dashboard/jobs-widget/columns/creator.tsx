4855d88683dd87fa28df7c7b286ceb3cdb5518a2
simke9445 <simke9445@gmail.com>
2023-06-27 14:51:58 +0200

    copy creator address on click in job widget

--- apps/warp-protocol/src/pages/dashboard/jobs-widget/columns/creator.tsx
+++ apps/warp-protocol/src/pages/dashboard/jobs-widget/columns/creator.tsx
@@ -5,12 +5,15 @@ import { Text } from 'components/primitives';
 import styles from './columns.module.sass';
 import { Job } from 'types/job';
 import { truncateAddress } from '@terra-money/apps/utils';
+import { useCopy } from 'hooks';
 
 export const CreatorCellRenderer = (cellProps: TableCellProps) => {
   const entry = cellProps.rowData as Job;
 
+  const copy = useCopy('address', entry.info.owner);
+
   return (
-    <Text className={classNames(styles.col, styles.col_creator)} variant="text">
+    <Text className={classNames(styles.col, styles.col_creator)} variant="text" onClick={copy}>
       {truncateAddress(entry.info.owner, [8, 6])}
     </Text>
   );
