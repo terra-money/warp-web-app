0a9e034486dc22e5dfdf2c1607a1c72a2b2a8253
simke9445 <simke9445@gmail.com>
2023-07-24 22:48:42 +0200

    temporary analytics coming soon overlay

--- apps/warp-protocol/src/pages/dashboard/analytics/Analytics.tsx
+++ apps/warp-protocol/src/pages/dashboard/analytics/Analytics.tsx
@@ -2,13 +2,17 @@ import styles from './Analytics.module.sass';
 import { Container, UIElementProps } from '@terra-money/apps/components';
 import { Charts } from './charts/Charts';
 import classNames from 'classnames';
+import { useChainSelector } from '@terra-money/apps/hooks';
 
 export const Analytics = (props: UIElementProps) => {
   const { className } = props;
 
+  const { selectedChain } = useChainSelector();
+
   return (
     <Container direction="column" className={classNames(styles.root, className)}>
       <Charts />
+      {selectedChain.name === 'injective' && <div className={styles.overlay}>Coming Soon</div>}
     </Container>
   );
 };
