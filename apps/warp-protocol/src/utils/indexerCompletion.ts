b8ab50c3bb80ae96f2e73787f3ffe008e1902ce0
simke9445 <simke9445@gmail.com>
2023-08-15 01:48:44 +0200

    temporaril disable indexer completion wait

--- apps/warp-protocol/src/utils/indexerCompletion.ts
+++ apps/warp-protocol/src/utils/indexerCompletion.ts
@@ -1,7 +1,7 @@
-import { sleep } from '@terra-money/apps/utils';
+// import { sleep } from '@terra-money/apps/utils';
 import { TX_KEY } from 'tx';
-import { fetchIndexerState } from './fetchIndexerState';
-import { min } from 'd3-array';
+// import { fetchIndexerState } from './fetchIndexerState';
+// import { min } from 'd3-array';
 
 interface IndexerCompletionOptions {
   networkName: string;
@@ -13,28 +13,29 @@ interface IndexerCompletionOptions {
 }
 
 // TODO: could be smarter here and map the required indexers to TX_KEY
-const INDEXER_KEYS = ['indexer:warp-jobs', 'indexer:warp-jobs-tx-history'];
+// const INDEXER_KEYS = ['indexer:warp-jobs', 'indexer:warp-jobs-tx-history'];
 
 export const indexerCompletion = async (options: IndexerCompletionOptions): Promise<void> => {
-  const { networkName, height, timeout = 10000, callback, chainName } = options;
+  // const { networkName, height, timeout = 10000, callback, chainName } = options;
+  const { callback } = options;
 
-  const now = Date.now();
+  // const now = Date.now();
 
-  while (Date.now() < now + timeout) {
-    const state = await fetchIndexerState(networkName, chainName);
+  // while (Date.now() < now + timeout) {
+  //   const state = await fetchIndexerState(networkName, chainName);
 
-    // wait for all of the required indexers to process the block
-    const minHeight = min(
-      state.filter((s) => INDEXER_KEYS.includes(s.pk)),
-      (v) => v.height
-    );
+  //   // wait for all of the required indexers to process the block
+  //   const minHeight = min(
+  //     state.filter((s) => INDEXER_KEYS.includes(s.pk)),
+  //     (v) => v.height
+  //   );
 
-    if (minHeight && minHeight >= height) {
-      break;
-    }
+  //   if (minHeight && minHeight >= height) {
+  //     break;
+  //   }
 
-    await sleep(1000);
-  }
+  //   await sleep(1000);
+  // }
 
   callback();
 };
