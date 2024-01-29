16b3ea543f1a964e1c9c67cfd6c24748c7905e0e
simke9445 <simke9445@gmail.com>
2024-01-29 17:50:38 +0100

    add layout banner + migrate button

--- apps/warp-protocol/src/components/layout/Layout.tsx
+++ apps/warp-protocol/src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 import { Sidebar } from './Sidebar';
 import styles from './Layout.module.sass';
 import { ReactComponent as BackgroundWrap } from 'components/assets/BackgroundWrap.svg';
-import { Text } from 'components/primitives';
+import { Button, Text } from 'components/primitives';
 import { useChainSelector } from '@terra-money/apps/hooks';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { useChainSelectorDialog } from 'components/dialog/chain-selector/ChainSelectorDialog';
@@ -33,26 +33,38 @@ export const Layout = ({ children }: LayoutProps) => {
   const isMobile = useMediaQuery('(max-width: 768px)');
 
   return (
-    <div className={styles.root}>
-      {isMobile ? (
-        <MobileWarning />
-      ) : (
-        <>
-          <BackgroundWrap className={styles.background_wrap} />
-          <Text variant="label" className={styles.protocol_name}>
-            Warp protocol
+    <>
+      {!isMobile && (
+        <div className={styles.topBanner}>
+          <Text variant="text" className={styles.migrate_text}>
+            Warp v2 is out, proceed with creating new jobs on latest version - v1 will sunset its support by 01.04.2024
           </Text>
-          <div className={styles.chain_selector} onClick={() => openChainSelectorDialog({})}>
-            <div className={styles.chain_icon}>{selectedChain.icon}</div>
-            <Text variant="text" className={styles.chain_text}>
-              {selectedChain.name}
-            </Text>
-            <KeyboardArrowDownIcon className={styles.chevron} />
-          </div>
-          <Sidebar className={styles.sidebar} />
-          <div className={styles.content}>{children}</div>
-        </>
+          <Button variant="secondary" className={styles.migrate_button}>
+            Migrate
+          </Button>
+        </div>
       )}
-    </div>
+      <div className={styles.root}>
+        {isMobile ? (
+          <MobileWarning />
+        ) : (
+          <>
+            <BackgroundWrap className={styles.background_wrap} />
+            <Text variant="label" className={styles.protocol_name}>
+              Warp protocol
+            </Text>
+            <div className={styles.chain_selector} onClick={() => openChainSelectorDialog({})}>
+              <div className={styles.chain_icon}>{selectedChain.icon}</div>
+              <Text variant="text" className={styles.chain_text}>
+                {selectedChain.name}
+              </Text>
+              <KeyboardArrowDownIcon className={styles.chevron} />
+            </div>
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.content}>{children}</div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
