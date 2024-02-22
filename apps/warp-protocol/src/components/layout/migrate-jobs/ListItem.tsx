5b7edca884d99a40dffadb4f4ae3979f92adf96e
simke9445 <simke9445@gmail.com>
2024-02-22 17:28:49 +0100

    migrate jobs changes

--- apps/warp-protocol/src/components/layout/migrate-jobs/ListItem.tsx
+++ apps/warp-protocol/src/components/layout/migrate-jobs/ListItem.tsx
@@ -1,8 +1,8 @@
-import { Text, Throbber } from 'components/primitives';
+import { Text } from 'components/primitives';
 import { ListChildComponentProps } from 'react-window';
 import { ListData } from './ListData';
 import styles from './ListItem.module.sass';
-import { useMigrateJobTx } from 'tx/useMigrateJobTx';
+import { useMigrateJobDialog } from './migrate-job/MigrateJobDialog';
 
 export const ListItem = (props: ListChildComponentProps<ListData>) => {
   const {
@@ -13,7 +13,7 @@ export const ListItem = (props: ListChildComponentProps<ListData>) => {
 
   const job = jobs[index];
 
-  const [txResult, migrateJobTx] = useMigrateJobTx(true);
+  const openMigrateJobsDialog = useMigrateJobDialog();
 
   return (
     <div
@@ -21,21 +21,16 @@ export const ListItem = (props: ListChildComponentProps<ListData>) => {
       className={styles.listItem}
       style={style}
       onClick={() => {
-        if (!txResult.loading) {
-          migrateJobTx({ job });
-        }
+        openMigrateJobsDialog({ job });
       }}
     >
       <Text className={styles.name} variant="text" weight="bold">
         {job.info.name}
       </Text>
-      {txResult.loading ? (
-        <Throbber dotClassName={styles.throbber} variant="secondary" size="small" />
-      ) : (
-        <Text className={styles.id} variant="text" weight="bold">
-          ID#{job.info.id}
-        </Text>
-      )}
+      <Text className={styles.id} variant="text" weight="bold">
+        ID#{job.info.id}
+      </Text>
+      )
     </div>
   );
 };
